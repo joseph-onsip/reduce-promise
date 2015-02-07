0c00e624b2601a4e2f7694cab354b5972fcfa3dc
Joseph Frazier <joseph@onsip.com>
2015-02-07 01:55:10 -0500

    use promiseArguments helper function

--- index.js
+++ index.js
@@ -7,24 +7,22 @@ module.exports.reduce = module.exports;
 module.exports.reduceRight = promisifyReduceMethod.bind(null, 'reduceRight');
 
 function promisifyReduceMethod (methodName, callback, initialValue) {
-  var args = [promisifyReducer(callback)];
+  var args = [promiseArguments(callback)];
   if (arguments.length > 2) {
     args.push(initialValue);
   }
 
-  return function (arrayPromise) {
-    return Promise.resolve(arrayPromise).then(function (array) {
-      return array[methodName].apply(array, args);
-    });
-  };
+  return promiseArguments(function (array) {
+    return array[methodName].apply(array, args);
+  });
 }
 
-function promisifyReducer (callback) {
-  return function promisifiedReducer (previousValue, currentValue, index, array) {
-    return Promise.all([previousValue, currentValue]).then(function (values) {
-      var previousValue = values[0];
-      var currentValue = values[1];
-      return callback(previousValue, currentValue, index, array);
-    });
+function promiseArguments (f) {
+  if (typeof f !== 'function') {
+    return f;
   }
+  return function () {
+    var args = Array.prototype.map.call(arguments, promiseArguments);
+    return Promise.all(args).then(f.apply.bind(f, null));
+  };
 }
